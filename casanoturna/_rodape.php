c78eff1deb56bef7640e6738abb60150290ce2bc
Pedro Naves <sbiligui@gmail.com>
2011-12-01 01:48:19 -0200

    pedrin teste

---
+++ casanoturna/_rodape.php
@@ -0,0 +1,18 @@
+<div id="Rodape">
+	<div class="campo_rodape">
+		<img src="public/img/logo.png" alt="" height="100" style="margin: 31px 20px 0 0; float:left;" />
+		<p>
+			<span class="vcard">
+				<span class="fn n">
+					<span class="given-name"></span>
+					<span class="additional-name"></span>
+					<span class="family-name"></span>
+				</span>
+				<span class="tel">(34)xxxx-xxx</span><br/>
+				<a class="email" href="mailto:contato@casanoturna.com.br">contato@casanoturna.com.br</a><br/><br/>
+			</span>
+			Copyright (c) 2011 - Todos os direitos reservados
+		</p>
+		<a class="externo" href=""><img src="public/img/Conix.png" alt="" width="141" style="margin:33px 0 0; float:right;" /></a>
+	</div>
+</div>
