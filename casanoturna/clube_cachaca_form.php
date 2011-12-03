b7b96f6b41aa77799dcb7dd99011a97dff350384
Pedro Naves <sbiligui@gmail.com>
2011-12-03 14:53:57 -0200

    comitando para atualizar

--- casanoturna/clube_cachaca_form.php
+++ casanoturna/clube_cachaca_form.php
@@ -1,3 +1,29 @@
+<?php
+	include("public/lib/_conexao.php");
+
+	$conexao = new Conexao;
+	$conexao->open();
+	
+	if(isset($_GET['string'])) {	
+		$string = $_GET['string'];		
+	}
+	
+	$busca = (isset($string)) ? " WHERE a.nome LIKE '%$string%' " : "";
+	
+	$clientes = $conexao->result("
+		SELECT 
+			c.nome as nome_clube,
+			b.id_clube,
+			a.*
+		FROM cliente as a
+		LEFT JOIN socio as b ON a.id = b.id_cliente
+		LEFT JOIN clube as c ON c.id = b.id_clube
+		$busca
+	");
+	
+	$conexao->close();
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
@@ -14,6 +40,8 @@
 		
 		<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="public/css/padrao.css" />
+		<link rel="stylesheet" type="text/css" href="public/css/formulario.css" />
+		<link rel="stylesheet" type="text/css" href="public/css/tabela.css" />
 		
 		<script type="text/javascript" src="public/js/jquery-1.7.min.js"></script>
 		<script type="text/javascript" src="public/js/superfish.js"></script>
@@ -45,7 +73,63 @@
 				<img src="public/img/casa noturna.jpg" alt="" width="900" height="180" />
 			</div>		
 			<div class="conteudo">
-				<div class="campo_conteudo"></div>
+				<div class="campo_conteudo"><h1>Associar Clientes - Cacha&ccedil;a</h1>
+
+					<div class="campo_botoes">
+						<form action="" class="busca">
+							<fieldset>
+								<span class="bg_busca">
+									<input type="text" value="" class="busca" id="" />
+								</span>
+								<input type="button" value="" id="" />
+							</fieldset>
+						</form>
+					</div>
+
+					<form action="">
+						<fieldset>
+							<table border="0">
+								<thead>
+									<tr>
+										<th><input type="checkbox" /></th>
+										<th>ID</th>
+										<th>NOME</th>										
+										<th>CLUBE</th>
+										<th class="opcoes">OP&Ccedil;&Otilde;ES</th>
+									</tr>
+								</thead>							
+								<tbody>
+									<?php foreach($clientes as $c => $cliente){
+										if($c%2==0) {?>
+									<tr>
+										<td><input type="checkbox" /></td>
+										<td><?=$cliente['id']?></td>
+										<td><?=$cliente['nome']?></td>
+										<td><a href="#"><?=$cliente['nome_clube']?></a></td>
+										<td>
+											<a href="cliente_form.php?i=<?=$cliente['id']?>"><img src="public/img/icon_editar.png" alt="" title="Editar" width="16" height="16" />
+											<a href="#"><img src="public/img/icon_log.png" alt="" title="Log" width="16" height="16" />
+											<a href="javascript:deleta_cliente(<?=$cliente['id']?>);" ><img src="public/img/icon_deletar.png" alt="" title="Remover" width="16" height="16" />
+										</td>
+									</tr>
+									<?php } else { ?>
+									<tr class="impar">
+										<td><input type="checkbox" /></td>
+										<td><?=$cliente['id']?></td>
+										<td><?=$cliente['nome']?></td>
+										<td><a href="#"><?=$cliente['nome_clube']?></a></td>
+										<td>
+											<a href="cliente_form.php?i=<?=$cliente['id']?>"><img src="public/img/icon_editar.png" alt="" title="Editar" width="16" height="16" />
+											<a href="#"><img src="public/img/icon_log.png" alt="" title="Log" width="16" height="16" />
+											<a href="javascript:deleta_cliente(<?=$cliente['id']?>);" ><img src="public/img/icon_deletar.png" alt="" title="Remover" width="16" height="16" />
+										</td>
+									</tr>
+									<? } } ?>
+								</tbody>
+							</table>
+						</fieldset>
+					</form>
+				</div>
 			</div>
 		</div>
 		
